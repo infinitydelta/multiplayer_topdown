9457f5c9e622114dce6fd6944b35272d053f0d85
Kyle San <ksan@usc.edu>
2015-09-20 19:01:36 -0700

    multiplayer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	public GameObject cam;
+
+	public float speed = 2;
+
+	Transform thisTransform = null;
+	Rigidbody rb = null;
+
+	void Awake()
+	{
+		thisTransform = GetComponent<Transform>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+		{
+			cam.SetActive(false);
+			this.enabled = false;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey("w"))
+		{
+			rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+		}
+		if (Input.GetKey("s"))
+		{
+			rb.AddForce(-Vector3.forward * speed, ForceMode.Impulse);
+		}
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(Vector3.left * speed, ForceMode.Impulse);
+		}
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(-Vector3.left * speed, ForceMode.Impulse);
+		}
+
+		if (Input.GetKey("left"))
+		{
+			thisTransform.Rotate(0, 1, 0);
+		}
+
+
+		Vector3.ClampMagnitude(rb.velocity, 10);
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/SyncTransform.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SyncTransform : NetworkBehaviour {
+
+
+	[SyncVar]
+	public Vector3 syncPosition;
+	[SyncVar]
+	public Quaternion syncRotation;
+
+	float timer = 0;
+
+	Transform thisTransform = null;
+
+	void Awake()
+	{
+		thisTransform = GetComponent<Transform>();
+		syncPosition = thisTransform.position;
+		syncRotation = thisTransform.rotation;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		UpdateTransform();
+		if (!isLocalPlayer)
+		{
+			thisTransform.position = syncPosition;
+			thisTransform.rotation = syncRotation;
+		}
+	}
+
+	//every 1/120 sec send position and rotation to server
+	//server reads network input and redistributes
+	[Command]
+	void CmdUpdateTransform(Vector3 pos, Quaternion rot)
+	{
+		syncPosition = pos;
+		syncRotation = rot;
+	}
+
+	[ClientCallback]
+	void UpdateTransform()
+	{
+		if (isLocalPlayer)
+		{
+			if (timer > 1/120)
+			{
+				CmdUpdateTransform(thisTransform.position, thisTransform.rotation);
+				timer = 0;
+			}
+			timer += Time.deltaTime;
+		}
+	}
+}
