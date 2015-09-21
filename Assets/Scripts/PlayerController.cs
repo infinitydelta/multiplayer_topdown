3932ca63e6994936d7ff909c3cb9eb17d4a34be5
Nathan Iskandar <jaiskand@usc.edu>
2015-09-20 21:54:17 -0700

    Mouse rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,27 @@ public class PlayerController : NetworkBehaviour {
 	public GameObject cam;
 
 	public float speed = 2;
+    public float rotSpeed = 180f;
 
 	Transform thisTransform = null;
 	Rigidbody rb = null;
+
+    Vector3 mousePosInWorldSpace;
+    float targetYRotation;
+    Camera playerCamera;
 
 	void Awake()
 	{
 		thisTransform = GetComponent<Transform>();
 		rb = GetComponent<Rigidbody>();
+        playerCamera = cam.GetComponent<Camera>();
 	}
 
 	// Use this for initialization
 	void Start () {
 		if (!isLocalPlayer)
 		{
-			cam.GetComponent<Camera>().enabled = false;
+			playerCamera.enabled = false;
 			cam.SetActive(false);
 			this.enabled = false;
 
@@ -29,6 +35,23 @@ public class PlayerController : NetworkBehaviour {
 	}
 	
 	// Update is called once per frame
+    void Update()
+    {
+        //mousePosInWorldSpace = playerCamera
+        Ray mouseRay = playerCamera.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        RaycastHit hit;
+        int layermask = 1 << 8; //only hit layer 8 (Floor)
+
+        if(Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit, 100f, layermask))
+        {
+            mousePosInWorldSpace = hit.point;
+            targetYRotation = Mathf.Atan2(-(mousePosInWorldSpace.z - thisTransform.position.z), mousePosInWorldSpace.x - thisTransform.position.x)*Mathf.Rad2Deg;
+            //Debug.Log(targetYRotation);
+        }
+
+
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction * 100, Color.yellow);
+    }
 	void FixedUpdate () {
 		if (Input.GetKey("w"))
 		{
@@ -52,8 +75,11 @@ public class PlayerController : NetworkBehaviour {
 			thisTransform.Rotate(0, 1, 0);
 		}
 
-
 		Vector3.ClampMagnitude(rb.velocity, 10);
+
+        thisTransform.rotation = Quaternion.Euler(0, targetYRotation, 0);
+
+        //thisTransform.LookAt(mousePosInWorldSpace);
 	}
 
 
